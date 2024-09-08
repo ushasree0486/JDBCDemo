e8fc3787af42dff4453cdfb69519aa1bd8f33035
Usha J <ushasree.jpl@gmail,.com>
2024-09-08 09:54:52 +0530

    Adding JDBCPreapedStatementDemo

---
+++ src/main/java/org/example/JDBCPreparedStatementDemo.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class JDBCPreparedStatementDemo {
+    public static void main(String[] args) throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver"); //load the driver with Connector J driver
+        String url = "jdbc:mysql://localhost:3306/mydb";
+        String uname = "root";
+        String password = "Admin";
+        ResultSet resultSet = null;
+        Connection con = null;
+        String query = "selet * from employee";
+        String inserQurey = "insert into employee values('3', 'Barkha' ,'Wembley');";
+
+        try {
+            con = DriverManager.getConnection(url, uname, password); //get the connection with Driver manager
+            Statement st = con.createStatement();
+            int count = st.executeUpdate(inserQurey);
+            System.out.println("No of rows affected == " + count);
+        } finally {
+            if (resultSet != null)
+                resultSet.close();
+            if (con != null)
+                con.close();
+        }
+    }
+}
